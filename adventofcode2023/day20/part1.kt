8761a98604633ca3f6badfa7d497cfa4ac04df32
julianbadillo <julianbadillo@gmail.com>
2023-12-24 14:36:17 -0600

    :pick: day 20

---
+++ adventofcode2023/day20/part1.kt
@@ -0,0 +1,92 @@
+import java.io.File
+import java.io.BufferedReader
+import java.util.PriorityQueue
+
+
+
+data class Gate(val origin: String, val op: String, val dest: List<String>) {
+    var on: Boolean = false
+    var inputs: MutableMap<String, Boolean> = mutableMapOf<String, Boolean>()
+    /* Receive a pulse (true=high, false=low), and calculate result 
+        to be passed to destinations (true=high, false=low, null=none) */
+    fun receivePulse(pulse: Boolean, from: String) : Boolean? {
+        // broadcast
+        when (op) {
+            "B" -> return pulse
+            "%" -> 
+                if (!pulse) {
+                    on = !on
+                    return on
+                }
+            "&" -> {
+                inputs[from] = pulse
+                //println("$inputs -> ${inputs.values}}")
+                return !inputs.values.all{it}
+            }
+        }
+        return null
+    }
+    override fun toString() = "($origin, $op, $dest [on: $on, $inputs])"
+}
+
+
+
+fun main(args: Array<String>) {
+    val reader = File(args[0]).bufferedReader()
+    val n = args[1].toInt()
+    val gates = reader.readLines().map { parseGate(it) }
+    // count inputs for conjunctions
+    gates.filter { it.op == "&" }
+        .forEach { 
+            gates.filter{ g -> g.dest.contains(it.origin) }
+                .forEach{ g -> it.inputs[g.origin] = false }
+        }
+
+    // map them all
+    val map = gates.map{ it.origin to it }.toMap()
+    //gates.forEach{ println(it) }
+    // println("inputs = ${map["inv"]?.inputs}")
+    // println("inputs = ${map["con"]?.inputs}")
+    var totalHigh = 0
+    var totalLow = 0
+    for (i in 1..n) {
+        //println("-- Push button $i --")
+        val (high, low) = pushButton(map)
+        totalHigh += high
+        totalLow += low
+    }
+    println("Total = ${totalHigh} x ${totalLow} = ${totalHigh * totalLow}")
+}
+
+fun parseGate(line: String): Gate {
+    val parts = line.split(" -> ")
+    return if (parts[0] == "broadcaster") Gate("broadcast", "B", parts[1].split(", "))
+            else Gate(parts[0].drop(1), parts[0].take(1), parts[1].split(", "))
+}
+
+data class GP(val origin: String, val dest: String, val pulse: Boolean)
+data class R(var high: Int, var low: Int)
+fun pushButton(map: Map<String, Gate>): R {
+    // push a button
+
+    var queue = ArrayDeque<GP>()
+    queue.addLast(GP("button", "broadcast", false)) // send a low pulse to broadcast
+    var result = R(high=0, low=1) // count the button pulse
+    while (queue.size > 0) {
+        val (origin, dest, pulse) = queue.removeFirst()
+        // println("GP($origin, $dest, $pulse)")
+        val gate = map[dest]
+        if (gate == null) continue
+        val res = gate.receivePulse(pulse, origin)
+        if (res != null) {
+            // count pulses
+            for (dest2 in gate.dest) {
+                if (res) result.high++ else result.low++
+                queue.addLast(GP(dest, dest2, res))
+                // println("$dest -${if (res) "high" else "low"}-> $dest2")
+            }
+        }
+    }
+    // println("Low pulses=$low, Highpulses=$high")
+    return result
+}
---
+++ adventofcode2023/day20/part2.kt
@@ -0,0 +1,109 @@
+import java.io.File
+import java.io.BufferedReader
+import java.util.PriorityQueue
+
+
+
+data class Gate(val origin: String, val op: String, val dest: List<String>) {
+    var on: Boolean = false
+    var inputs: MutableMap<String, Boolean> = mutableMapOf<String, Boolean>()
+    /* Receive a pulse (true=high, false=low), and calculate result 
+        to be passed to destinations (true=high, false=low, null=none) */
+    fun receivePulse(pulse: Boolean, from: String) : Boolean? {
+        // broadcast
+        when (op) {
+            "B" -> return pulse
+            "%" -> 
+                if (!pulse) {
+                    on = !on
+                    return on
+                }
+            "&" -> {
+                inputs[from] = pulse
+                //println("$inputs -> ${inputs.values}}")
+                return !inputs.values.all{it}
+            }
+        }
+        return null
+    }
+    override fun toString() = "($origin, $op, $dest [on: $on, $inputs])"
+}
+
+
+
+fun main(args: Array<String>) {
+    val reader = File(args[0]).bufferedReader()
+    val n = args[1].toInt()
+    val gates = reader.readLines().map { parseGate(it) }
+    // count inputs for conjunctions
+    gates.filter { it.op == "&" }
+        .forEach { 
+            gates.filter{ g -> g.dest.contains(it.origin) }
+                .forEach{ g -> it.inputs[g.origin] = false }
+        }
+
+    // map them all
+    val map = gates.map{ it.origin to it }.toMap()
+    printRecursive(map, 0, "broadcast")
+    //gates.forEach{ println(it) }
+    // println("inputs = ${map["inv"]?.inputs}")
+    // println("inputs = ${map["con"]?.inputs}")
+    
+    // for (i in (1..n)) {
+    //     //println("-- Push button $i --")
+    //     if (pushButton(map)) {
+    //         println("Total = ${i}")
+    //         break
+    //     } else if (i % 1000 == 0 ) {
+    //         println("$i")
+    //     }
+    // }
+}
+
+fun parseGate(line: String): Gate {
+    val parts = line.split(" -> ")
+    return if (parts[0] == "broadcaster") Gate("broadcast", "B", parts[1].split(", "))
+            else Gate(parts[0].drop(1), parts[0].take(1), parts[1].split(", "))
+}
+
+fun printRecursive(map: Map<String, Gate>, level: Int, node: String) {
+    if (level > 4) return
+    print(Array<Char>(level){' '}.joinToString(""))
+    val gate = map[node]
+    if (gate == null) {
+        println(node)
+        return
+    }
+    println(gate)
+    for (d in gate.dest){
+        printRecursive(map, level+1, d)
+    }
+}
+
+data class GP(val origin: String, val dest: String, val pulse: Boolean)
+fun pushButton(map: Map<String, Gate>): Boolean {
+    // push a button
+
+    var queue = ArrayDeque<GP>()
+    queue.addLast(GP("button", "broadcast", false)) // send a low pulse to broadcast
+    while (queue.size > 0) {
+        val (origin, dest, pulse) = queue.removeFirst()
+        // println("GP($origin, $dest, $pulse)")
+        val gate = map[dest]
+        if (gate == null) continue
+        val res = gate.receivePulse(pulse, origin)
+        if (res != null) {
+            // count pulses
+            for (dest2 in gate.dest) {
+                if (dest2 == "rx") {
+                    // println("$dest -${if (res) "high" else "low"}-> $dest2") 
+                    if (!res) return true
+                }
+                queue.addLast(GP(dest, dest2, res))
+                // println("$dest -${if (res) "high" else "low"}-> $dest2")
+            }
+        }
+    }
+    // println("Low pulses=$low, Highpulses=$high")
+    return false
+}
