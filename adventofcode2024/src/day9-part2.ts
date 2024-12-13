e864e28775fc8af60698a14fc1326f2fc4aad45d
julianbadillo <julianbadillo@gmail.com>
2024-12-12 19:36:37 -0600

    :broom: simpler

--- adventofcode2024/src/day9-part2.ts
+++ adventofcode2024/src/day9-part2.ts
@@ -3,32 +3,26 @@ import { Block, parseBlocks } from './day9-part1';
 
 export const squeezeFull = (blocks: Block[]): Block[] => {
     let i = 0;
-    const files = blocks.filter(b => b.id !== null);
-    let j = files.length - 1;
-    while (j > 0) {
-        const file = files[j];
-        // find space
-        for (i = 0; i < blocks.length && (blocks[i].id !== null || file.size > blocks[i].size); i++);
-        // block doesn't fit any empty space
-        if (i >= blocks.length || blocks[i].pos >= file.pos) {
-            j--;
-            continue;
-        }
-        const space = blocks[i];
-        // insert a block
-        blocks.splice(i, 0, new Block(file.id, space.pos, file.size));
-        // reduce space
-        space.size -= file.size;
-        space.pos += file.size;
-        // file becomes empty space
-        file.id = null;
-        if (space.size == 0) {
-            // remove space block
-            blocks.splice(i + 1, 1);
-        }
-        j--;
-        // console.log(blocks.map(b => b.print()).join(''));
-    }
+    blocks.filter(b => b.id !== null)
+        .reverse()
+        .forEach(file => {
+            // find space
+            for (i = 0; i < blocks.length && (blocks[i].id !== null || file.size > blocks[i].size); i++);
+            // block doesn't fit any empty space
+            if (i >= blocks.length || blocks[i].pos >= file.pos) return;
+            const space = blocks[i];
+            // insert a block
+            blocks.splice(i, 0, new Block(file.id, space.pos, file.size));
+            // reduce space
+            space.size -= file.size;
+            space.pos += file.size;
+            // file becomes empty space
+            file.id = null;
+            if (space.size == 0) {
+                // remove space block
+                blocks.splice(i + 1, 1);
+            }
+        });
     return blocks;
 }
 
